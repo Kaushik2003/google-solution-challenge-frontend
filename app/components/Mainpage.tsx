42d7dd63bfa9b7a57b15ec42df2e208882bfd29c
Kaushik Samadder <70917815+Kaushik2003@users.noreply.github.com>
2025-01-30 14:02:54 +0530

    minor changes to CSS of the Mainpage

--- app/components/Mainpage.tsx
+++ app/components/Mainpage.tsx
@@ -4,23 +4,22 @@ import { TextGenerateEffect } from "./ui/text-generate-effect";
 import { Button } from "./ui/Button";
 import { FaLocationArrow } from "react-icons/fa";
 
-const words = `Next Gen Teacher Assistant`;
+const words = `Automate Grading. Personalize Feedback. Simplify Teaching`;
 
 const Mainpage = () => {
   return (
     <BackgroundGradientAnimation>
       <div className="absolute z-50 inset-0 flex flex-col items-center justify-center text-white font-bold px-4 pointer-events-none text-4xl text-center md:text-4xl lg:text-7xl">
         <div className="bg-clip-text text-transparent drop-shadow-2xl bg-gradient-to-b from-white/80 to-white/20">
-          Tattva
-          <TextGenerateEffect
-            className="text-center text-xl-2 "
-            duration={2}
-            words={words}
-          ></TextGenerateEffect>
+          Tattva - AI That Empowers Educators
         </div>
+        <TextGenerateEffect
+          className="text-center text-xl "
+          duration={2}
+          words={words}
+        ></TextGenerateEffect>
         <a href="#about">
           <Button
-            
             title="Launch Tattva"
             icon={<FaLocationArrow />}
             position="right"
